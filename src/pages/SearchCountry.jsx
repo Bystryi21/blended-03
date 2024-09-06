8673858c280aa29d22dfae7adb26a3cbd755a195
Pavlo-Bielohurov <158511567+Pavlo-Bielohurov@users.noreply.github.com>
2024-09-06 21:36:53 +0300

    Search

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,41 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryList,
+  Heading,
+  SearchForm,
+  Section,
+} from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from '../service/countryApi';
 
 export const SearchCountry = () => {
+  const [region, setRegion] = useState('');
+  const [countres, setCountres] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!region) return;
+    async function search() {
+      try {
+        const respons = await fetchByRegion(region);
+        setCountres(respons);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    search();
+  }, [region]);
+
+  const handleChangeRegion = region => {
+    setRegion(region);
+  };
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {error && <Heading title={error} bottom />}
+        <SearchForm onSubmit={handleChangeRegion} />
+        <CountryList country={countres} />
       </Container>
     </Section>
   );
